4fd776edfc99215894eefb095c2c39b25504257c
a.tamazyan <tamazyanarsen@focus.bi>
2020-10-22 08:57:03 +0300

    init jwt auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
     imports: [TypeOrmModule.forRoot({
         type: 'sqlite',
         database: './test.db'
-    })],
+    }), AuthModule, UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/providers/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { IUser } from "../models/user.interface";
-
-@Injectable()
-export class UserService {
-    public create(user: IUser) {
-        console.log(user);
-    }
-}
